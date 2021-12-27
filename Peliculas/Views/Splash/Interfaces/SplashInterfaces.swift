74265e845916d186e4216e218a88cd9cfa4c89f5
SaraFranciscoBarradoVueling <sara.francisco@col.vueling.com>
2021-12-27 07:55:12 +0100

    Splash Screen created

---
+++ Peliculas/Views/Splash/Interfaces/SplashInterfaces.swift
@@ -0,0 +1,8 @@
+//
+//  SplashInterfaces.swift
+//  Peliculas
+//
+//  Created by Sara Francisco on 27/12/21.
+//
+
+import Foundation
---
+++ Peliculas/Views/Splash/Presenter/SplashPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  SplashPresenter.swift
+//  Peliculas
+//
+//  Created by Sara Francisco on 27/12/21.
+//
+
+import Foundation
