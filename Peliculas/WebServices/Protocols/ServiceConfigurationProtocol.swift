58bcdfe425917f9476b07bf5b6468933d9ca0d68
SaraFranciscoBarradoVueling <sara.francisco@col.vueling.com>
2021-12-27 08:24:01 +0100

    network layer create

---
+++ Peliculas/WebServices/Protocols/ServiceConfigurationProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  ServiceConfigurationProtocol.swift
+//  Peliculas
+//
+//  Created by Sara Francisco on 27/12/21.
+//
+
+import Foundation
+
+internal protocol ServiceConfigurationProtocol {
+    var path: String { get }
+    var method: ServiceMethods { get }
+    var request: URLRequest? { get }
+    var parameters: [String: Any]? { get }
+}
